cd309dab8bf478b502d68241a41e6f5c2aec5127
carlosmilenaquesada <carlosmilenaquesada@gmail.com>
2024-02-08 14:51:45 +0000

    ultimo

--- src/conexion/ConexionCliente.java
+++ src/conexion/ConexionCliente.java
@@ -51,6 +51,7 @@ public class ConexionCliente {
                     }
                 }
             }).start();
+            
             new Thread(new Runnable() {
                 @Override
                 public void run() {
